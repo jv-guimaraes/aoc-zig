1dce6ef78e70faa80e9ea7d21fe226e65fb5df75
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-05-02 21:34:22 -0300

    2022/d3 - Solve part 2

--- 2022/d3/main.zig
+++ 2022/d3/main.zig
@@ -12,6 +12,7 @@ fn priority(char: u8) u8 {
 fn shared_item(sack: []const u8) ?u8 {
     const len = sack.len;
     var first_half = IntegerBitSet(123).initEmpty();
+
     for (sack[0 .. len / 2]) |char| {
         first_half.set(char);
     }
@@ -25,6 +26,23 @@ fn shared_item(sack: []const u8) ?u8 {
     return null;
 }
 
+fn shared_badge(sack1: []const u8, sack2: []const u8, sack3: []const u8) ?u8 {
+    var item_set1 = IntegerBitSet(123).initEmpty();
+    var item_set2 = IntegerBitSet(123).initEmpty();
+    for (sack1) |char| {
+        item_set1.set(char);
+    }
+    for (sack2) |char| {
+        item_set2.set(char);
+    }
+    for (sack3) |char| {
+        if (item_set1.isSet(char) and item_set2.isSet(char)) {
+            return char;
+        }
+    }
+    return null;
+}
+
 pub fn main() !void {
     var lines = std.mem.tokenize(u8, input, "\r\n");
     var sum: u32 = 0;
@@ -32,5 +50,17 @@ pub fn main() !void {
         const item = shared_item(line).?;
         sum += priority(item);
     }
-    std.debug.print("Part 1: {}", .{sum});
+    std.debug.print("Part 1: {}\n", .{sum});
+
+    // Part 2
+    lines = std.mem.tokenize(u8, input, "\r\n");
+    sum = 0;
+    while (lines.peek()) |_| {
+        const line1 = lines.next().?;
+        const line2 = lines.next().?;
+        const line3 = lines.next().?;
+        const item = shared_badge(line1, line2, line3).?;
+        sum += priority(item);
+    }
+    std.debug.print("Part 2: {}\n", .{sum});
 }
