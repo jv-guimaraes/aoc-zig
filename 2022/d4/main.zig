57ebe1bd06de0dde08d78689dbb12f97c9846ec4
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-05-04 16:18:51 -0300

    2022/d4 - Solver part 2

--- 2022/d4/main.zig
+++ 2022/d4/main.zig
@@ -6,30 +6,41 @@ const tokenize = std.mem.tokenize;
 const Range = struct {
     left: usize,
     right: usize,
+
     fn contains(self: Range, other: Range) bool {
         if (self.left <= other.left and self.right >= other.right) {
             return true;
         }
         return false;
     }
+
     fn new(left: usize, right: usize) Range {
         return Range{ .left = left, .right = right };
     }
+
     fn from_string(string: []const u8) Range {
         var tokens = tokenize(u8, string, "-");
         const left: usize = parseInt(usize, tokens.next().?, 10) catch unreachable;
         const right: usize = parseInt(usize, tokens.next().?, 10) catch unreachable;
         return Range.new(left, right);
     }
+
+    fn overlap(self: Range, other: Range) bool {
+        if (self.left >= other.left and self.left <= other.right) {
+            return true;
+        }
+        if (self.right >= other.left and self.right <= other.right) {
+            return true;
+        }
+        return false;
+    }
 };
 
 pub fn main() !void {
-    var lines = tokenize(u8, input, "\r\n");
-
     // Part 1
+    var lines = tokenize(u8, input, "\r\n");
     var count: usize = 0;
     while (lines.next()) |line| {
-        // 6-6,4-6
         var split = tokenize(u8, line, ",");
         const r1 = Range.from_string(split.next().?);
         const r2 = Range.from_string(split.next().?);
@@ -37,5 +48,18 @@ pub fn main() !void {
             count += 1;
         }
     }
-    std.debug.print("Part 1: {}", .{count});
+    std.debug.print("Part 1: {}\n", .{count});
+
+    // Part 2
+    lines.reset();
+    count = 0;
+    while (lines.next()) |line| {
+        var split = tokenize(u8, line, ",");
+        const r1 = Range.from_string(split.next().?);
+        const r2 = Range.from_string(split.next().?);
+        if (r1.overlap(r2) or r2.overlap(r1)) {
+            count += 1;
+        }
+    }
+    std.debug.print("Part 2: {}\n", .{count});
 }
